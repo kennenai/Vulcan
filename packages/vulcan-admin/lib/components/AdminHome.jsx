58caa65ee6da1e1e44072f2d4e750ceb3bf465ee
Jim Wrubel <jwrubel@gmail.com>
2019-01-19 15:44:30 -0500

    add a scripts option to admin

--- packages/vulcan-admin/lib/components/AdminHome.jsx
+++ packages/vulcan-admin/lib/components/AdminHome.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Components, withCurrentUser, AdminColumns } from 'meteor/vulcan:core';
 import { FormattedMessage } from 'meteor/vulcan:i18n';
 import Users from 'meteor/vulcan:users';
+import { Button, Header, Segment } from 'semantic-ui-react'
 
 import '../modules/columns.js';
+
+const fixPostDurations = () => {
+  Meteor.call('updateReadingTime')
+}
 
 const AdminHome = ({ currentUser }) =>
   <div className="admin-home page">
     <Components.ShowIf check={Users.isAdmin} document={currentUser} failureComponent={<p className="admin-home-message"><FormattedMessage id="app.noPermission" /></p>}>
-      <Components.Datatable 
-        collection={Users} 
-        columns={AdminColumns} 
+      <Components.Datatable
+        collection={Users}
+        columns={AdminColumns}
         options={{
           fragmentName: 'UsersAdmin',
           terms: {view: 'usersAdmin'},
@@ -18,6 +23,10 @@ const AdminHome = ({ currentUser }) =>
         }}
         showEdit={true}
       />
+      <Segment>
+        <Header as='h3'>Scripts</Header>
+        <Button color='red' onClick={fixPostDurations}>Fix Post Durations</Button>
+      </Segment>
     </Components.ShowIf>
   </div>
 
